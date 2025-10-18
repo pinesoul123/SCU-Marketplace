cebb24cb78e6e69e32a9e8030231c4ccc220a5be
Isabelle <87943193+IGraceH@users.noreply.github.com>
2025-10-18 10:47:23 -0700

    Create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-//import logo from './logo.svg';
-//import './App.css';
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
-
-
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Feed from "./pages/Feed";
@@ -34,11 +7,12 @@ import Auth from "./pages/Auth";
 function App() {
   return (
     <BrowserRouter>
-      <nav style={{display:"flex",gap:12,padding:12}}>
+      <nav id="navbar">
         <Link to="/">Feed</Link>
         <Link to="/new">New Listing</Link>
         <Link to="/auth">Auth</Link>
       </nav>
+      
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="/new" element={<CreateListing />} />

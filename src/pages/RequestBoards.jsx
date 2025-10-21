ea0cf1b1caa427e4b40e018fd0fbdb1d5810d311
Isabelle <87943193+IGraceH@users.noreply.github.com>
2025-10-21 13:04:25 -0700

    added request boards

--- src/pages/RequestBoards.jsx
+++ src/pages/RequestBoards.jsx
@@ -1,7 +1,23 @@
+
+const boards = ["Graham", "Scipi"]
+
+function Board({ name }) {
+    return (
+        <a class="request-board-button" href="">
+            <h2>{name}</h2>
+        </a>
+    )
+}
+
 export default function RequestBoards() {
-  return (
-    <div id="content">
-      <h1>Request Boards</h1>
-    </div>
-  );
+    const renderBoards = boards.map(board => <Board name={board}></Board>);
+
+    return (
+        <div id="content">
+            <h1>Request Boards</h1>
+            <div id="request-boards-container">
+                {renderBoards}
+            </div>
+        </div>
+    );
 }

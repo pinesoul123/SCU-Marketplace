6e8b1a8f626db749b4109fbc8a3695a41f969518
Isabelle <87943193+IGraceH@users.noreply.github.com>
2025-10-22 14:38:52 -0700

    Created basic form

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,4 +1,5 @@
 import { createListing } from "../api/listings";
+import "../styles/CreateListing.css"
 
 export default function CreateListing() {
   async function onPublish(formValues, fileList) {
@@ -6,10 +7,35 @@ export default function CreateListing() {
     console.log("New listing created:", id);
   }
 
+  const listingForm = (
+    <form id="listing-form">
+      <div id="form-left">
+        <input type="file"></input>
+      </div>
+      <div id="form-right">
+        <label for="listing-name">Listing Name</label>
+        <br></br>
+        <input id="listing-name" class="textbox" type="text"></input>
+        <br></br><br></br>
+        <label for="listing-price">Price</label>
+        <br></br>
+        <input id="listing-price" class="textbox" type="text"></input>
+        <br></br><br></br>
+        <label for="listing-desc">Description</label>
+        <br></br>
+        <textarea id="listing-desc" class="textbox" ></textarea>
+      </div>
+      <div id="form-bottom">
+        <label for="listing-category">Category</label>
+        <label for="listing-condition">Condition</label>
+      </div>
+      </form>
+  );
+
   return (
     <div id="content">
       <h1>Create Listing</h1>
-      {/* add here */}
+      {listingForm}
     </div>
   );
 }

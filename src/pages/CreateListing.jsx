1c6550f81ad24cb4ee51d340fea7865e6b731e9d
Lydia Martin <134659141+bansheesh@users.noreply.github.com>
2025-10-21 22:55:42 -0700

    fixed blank screen problem

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,18 +1,4 @@
-import { createListing } from "../api/listings";
-
 export default function CreateListing() {
-  // backend hook-up for this page
-  // takes form data + images, sends to Firestore + Storage
-  async function onPublish(formValues, fileList) {
-    const id = await createListing(formValues, Array.from(fileList || []));
-    console.log("New listing created:", id);
-    //show a toast / navigate, etc.
-  }
-
-  return (
-    <div style={{ padding: 24 }}>
-      Create Listing page
-      {/* once the form is done, connect its submit handler to onPublish() */}
-    </div>
-  );
+  return <div style={{ padding: 24 }}>Create Listing page</div>;
 }
+

4db35b9e8efa7af4f2b199131591f10248b09f5d
Isabelle <87943193+IGraceH@users.noreply.github.com>
2025-10-22 15:01:44 -0700

    Created selection chips

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,11 +1,36 @@
 import { createListing } from "../api/listings";
+import { useState } from 'react';
 import "../styles/CreateListing.css"
+
+const categories = [1, 2, 3, 4, 5];
+
+function Selection({ options, selected, setSelected }) {
+  function handleClick(e, option) {
+    setSelected(option);
+    e.preventDefault();
+  }
+
+  const chips = [];
+  for (let option of options) {
+    if (option == selected) {
+      chips.push(<button key={option} onClick={(e) => e.preventDefault()}class="selected-chip">{option}</button>);
+    } else {
+      chips.push(<button key={option} onClick={(e) => handleClick(e, option)}>{option}</button>);
+    }
+  }
+
+  return (
+    <div>{chips}</div>
+  )
+}
 
 export default function CreateListing() {
   async function onPublish(formValues, fileList) {
     const id = await createListing(formValues, Array.from(fileList || []));
     console.log("New listing created:", id);
   }
+
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const listingForm = (
     <form id="listing-form">
@@ -27,6 +52,7 @@ export default function CreateListing() {
       </div>
       <div id="form-bottom">
         <label for="listing-category">Category</label>
+        <Selection options={categories} selected={selectedCategory} setSelected={setSelectedCategory} />
         <label for="listing-condition">Condition</label>
       </div>
       </form>

ddfc9077d828568df3c1cc8ac7d9852c75ade7b5
Lydia Martin <134659141+bansheesh@users.noreply.github.com>
2025-10-24 17:04:17 -0700

    updated auth.jsx

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { auth } from "../lib/firebase";
+import { useAuth } from "../lib/AuthProvider";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -8,33 +9,31 @@ import {
 
 //Handles user authentication (sign up, sign in, sign out)
 export default function Auth() {
+  const { user, loading } = useAuth();
   //Local state for user input and signed-in user data
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null);
 
   //Handles creating a new account and automatically signing in
   async function handleSignUp() {
     const userCred = await createUserWithEmailAndPassword(auth, email, password);
-    setUser(userCred.user);
     alert("Account created and signed in!");
   }
 
   //Handles logging in an existing user
   async function handleSignIn() {
     const userCred = await signInWithEmailAndPassword(auth, email, password);
-    setUser(userCred.user);
     alert("Signed in!");
   }
 
   //Handles logging out the current user
   async function handleSignOut() {
     await signOut(auth);
-    setUser(null);
     alert("Signed out!");
   }
 
   //below is all temporary, I just used this to get past the sign in portion when testing
+  if (loading) { return (<div style={{ padding: 24 }}><h1>Auth Page</h1><p>Checking login...</p></div>); }
   return (
     <div style={{ padding: 24 }}>
       <h1>Auth Page</h1>

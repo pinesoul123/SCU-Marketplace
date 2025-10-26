5ebe56a00ec73a69c188b0e536b08c257a01a25f
Isabelle <87943193+IGraceH@users.noreply.github.com>
2025-10-25 23:54:14 -0700

    spacing fix

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -94,10 +94,9 @@ function ListingsPage({listings, currentPage, itemsPerPage}) {
 
 export default function Market() {
   const [currentPage, setCurrentPage] = useState(0);
-  const itemsPerPage = 2;
+  const itemsPerPage = 12;
 
-  const tempListings = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
+  const tempListings = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
 
   return (
     <div id="content">

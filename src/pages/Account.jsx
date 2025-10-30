bdcad21d27ead40cfac944ec13e12b3c7b385b5a
Isabelle <87943193+IGraceH@users.noreply.github.com>
2025-10-29 23:28:49 -0700

    account my listings

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,4 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react"
+import ListingCard from "../components/ListingCard.jsx"
+import { getMyListings } from "../api/account";
+import "../styles/Account.css";
+
+function AccountInfo() {
+  return (
+    <div id="account-info-container">
+      <h1>Account</h1>
+      Name
+      Email
+    </div>
+  )
+}
+
+function MyListings() {
+  const [myListingDocs, setMyListingDocs] = useState([]);
+  const getListingDocs = getMyListings();
+  
+  useEffect(() => {
+    getListingDocs
+    .then(myListingDocs => {
+      setMyListingDocs(myListingDocs); 
+    });
+  }, [])
+  
+  const myListings = myListingDocs.map(listingDoc => <ListingCard key={listingDoc.id} listingData={listingDoc}></ListingCard>)
+
+  return (
+    <>
+      <div id="account-listings-container">
+        {myListings}
+      </div>
+    </>
+  )
+}
 
 class Account extends React.Component {
   // If you later want to load profile data, use lifecycle methods:
@@ -8,7 +44,10 @@ class Account extends React.Component {
   render() {
     return (
       <div id="content">
-        <h1>Account</h1>
+        <div id="account-container">
+          <AccountInfo />
+          <MyListings />
+        </div>
       </div>
     );
   }

523f92eff6eda2ed1c81d4ba6a51efc822702129
Isabelle <87943193+IGraceH@users.noreply.github.com>
2025-11-02 21:56:42 -0800

    removed testing images

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -35,7 +35,7 @@ function GalleryButton({step, content, images, activeImage, setActiveImage}) {
     }
     // Next button, only renders if active image is not last image
     if (step > 0) {
-        if (activeImage !== images.length - 1) {
+        if ((activeImage !== images.length - 1) && (activeImage != 0)) {
             return (button);
         }
     }
@@ -145,22 +145,11 @@ export default function Listing() {
         return;
     }
     const listingData = listingDoc.listing;
-    console.log("last-------------------------------------");
-
-
-    
-
-    // Test images
-    const imageURLs = [
-        "https://64.media.tumblr.com/ba526c636108f3b2df589bf44032fde3/ec9f3ab8fe0dd7d3-01/s2048x3072/bbeac1c26482b94c41c540fafce6629fe6e53483.png",
-        "https://64.media.tumblr.com/742d481df09a99e6f90473f8117740c3/4c0c86c3b89ad6a1-52/s2048x3072/4dd160e094669754ed6be8b13c1c90429f726e9a.png",
-        "https://64.media.tumblr.com/8bba5c35ddc0a53b61ed9224798169af/9d8b05082cc9b969-43/s2048x3072/17506cc221aba28a14fa3cec28929f788aba3d7a.png"
-    ]
 
     return (
         <div id="content">
             <div id="listing-container">
-                <ImageGallery imageURLs={imageURLs} />
+                <ImageGallery imageURLs={listingData.photoURLs} />
                 <div id="listing-info">
                     <p>${listingData.price}</p>
                     <h2>{listingData.title}</h2>

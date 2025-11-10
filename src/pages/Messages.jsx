12ba20acf1953a38bf844c8057a2c2c106ab9220
Isabelle <87943193+IGraceH@users.noreply.github.com>
2025-11-10 13:33:13 -0800

    added listing info to messages

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -9,24 +9,64 @@ async function getChats() {
     return (await fetchMyChats());
 }
 
-async function getListing(id) {
-    return (await listings.get(id));
+async function getChatsInfo(myChats) {
+    const chatInfo = new Map();
+    for (let chat of myChats) {
+        const listingId = chat.id.split("__")[0];
+        chatInfo.set(chat.id, await listings.get(listingId));
+    }
+    return chatInfo;
+}
+
+function ChatList({ myChats, currentChat, setCurrentChat }) {
+    const [myChatsInfo, setMyChatsInfo] = useState();
+    const getMyChatsInfo = getChatsInfo(myChats);
+
+    useEffect(() => {
+        getMyChatsInfo
+        .then(myChatsInfo => {
+            setMyChatsInfo(myChatsInfo);
+        })
+        .catch((error) => {
+            console.log("something went wrong");
+            console.log(error);
+        });
+    }, [])
+
+    if (myChatsInfo == null) {
+        return;
+    }
+
+    const chatList = [];
+
+    for (let chat of myChats) {
+        if (currentChat == chat.id) {
+            chatList.push(<button className="chat-list-item button active">
+                {myChatsInfo.get(chat.id).listing.title}
+                </button>)
+        } else {
+            chatList.push(<button className="chat-list-item button" 
+                onClick={() => setCurrentChat([chat.id, myChatsInfo.get(chat.id).listing.title])}>
+                    {myChatsInfo.get(chat.id).listing.title}
+                </button>)
+        }
+    }
+
+    return (
+        <div id="chat-list">{chatList}</div>
+    )
 }
 
 export default function Messages() {
-    const [currentChat, setCurrentChat] = useState("");
+    const [currentChat, setCurrentChat] = useState(["", ""]);
 
     const [myChats, setMyChats] = useState();
-    const [myChatList, setMyChatList] = useState([]);
-    const getMyChats = getChats();        
-    
-    const chatList = [];
+    const getMyChats = getChats();     
 
     useEffect(() => {
         getMyChats
         .then(myChats => {
             setMyChats(myChats);
-            console.log(myChats); 
         })
         .catch((error) => {
             console.log("something went wrong");
@@ -38,29 +78,17 @@ export default function Messages() {
         return;
     }
 
-    
-    for (let chat of myChats) {
-        // const listingId = chat.id.split("__")[0];
-        // getListing(listingId).then(data => {
-        //     chatList.push(<button className="chat-list-item button" onClick={() => setCurrentChat(chat.id)}>
-        //         {data.listing.title}
-        //         </button>);
-        //     // setMyChatList(chatList);
-        // });
-        // // console.log(listing);
-        chatList.push(<button className="chat-list-item button" onClick={() => setCurrentChat(chat.id)}>Chat</button>)
-    }
 
     return (
         <div id="content">
             <h1>Messages</h1>
             <div id="messages-page-container">
-                <div id="chat-list">{chatList}</div>
+                <ChatList myChats={myChats} currentChat={currentChat[0]} setCurrentChat={setCurrentChat} />
                 <div id="wide-chat-container">
                     <div id="wide-chat-heading">
-                        {currentChat && <Link to={"/listing?id=" + currentChat.split("__")[0]}>Listing</Link>}
+                        {currentChat && <Link to={"/listing?id=" + currentChat[0].split("__")[0]}>{currentChat[1]}</Link>}
                     </div>
-                    <Chat chatId={currentChat} chatActive={true} />
+                    <Chat chatId={currentChat[0]} chatActive={true} />
                 </div>
             </div>
         </div>
